54398387ca8b6b879bb6eb044d84199faaadfc9f
YJsh <syj2014mail@163.com>
2016-08-07 02:00:20 +0800

    获取图片地址并下载到本地

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
+# -*- coding: gbk -*-
 import os
 import re
+import json
+import time
+import urllib
 import urllib2
 import threading
 
 baseDirPath = "./"
 baseURL = "http://ac.qq.com/"
 comicBaseURL = "http://ac.qq.com/Comic/comicInfo/id/"
-comicId = 505430
 
 def dealWithSpecialCharacter(string, characters="\\/:*?\"<>|", substitute=" "):
     if "\"" in characters:
@@ -16,59 +18,119 @@ def dealWithSpecialCharacter(string, characters="\\/:*?\"<>|", substitute=" "):
     string = pattern.sub(substitute, string)
     return string
 
-def downloadImg(imgURL, imgDir, imgIndex):
-    imgPath = os.path.join(imgDir, "{0}.jpg".format(imgIndex))
-    urllib.urlretrieve(imgURL, imgPath, None) 
+def downloadPic(picURL, picDir, picIndex):
+    picPath = os.path.join(picDir, "{0}.jpg".format(picIndex))
+    urllib.urlretrieve(picURL, picPath, None) 
 
-def getChapterImgInfo(chapterURL):
-    print("getChapterImgInfo")
+# 获取章节图片数据
+def handleChapterInfo(data):
+    keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+    def UTF8Decode(c):
+        a = ""
+        b = d = c1 = c2 = 0
+        while b < len(c):
+            d = ord(c[b])
+            if 128 > d:
+                a += chr(d)
+                b += 1
+            elif 191 < d and 224 > d:
+                c2 = ord(c[b+1])
+                a += chr((d & 31) << 6 | c2 & 63)
+                b += 2
+            else:
+                c2 = ord(c[b+1])
+                c3 = ord(c[b+2])
+                a += chr((d & 15) << 12 | (c2 & 63) << 6 | c3 & 63)
+                b += 3
+        return a
+
+    def decode(c):
+        a = ""
+        b = d = h = f = g = e = 0
+        c = re.sub(r"[^\w\+\/\=]", "", c)
+        while e < len(c):
+            b = keyStr.index(c[e])
+            d = keyStr.index(c[e+1])
+            f = keyStr.index(c[e+2])
+            g = keyStr.index(c[e+3])
+            e = e + 4
+            b = b << 2 | d >> 4
+            d = (d & 15) << 4 | f >> 2
+            h = (f & 3) << 6 | g
+            a += chr(b)
+            if 64 != f:
+                a += chr(d)
+            if 64 != g:
+                a += chr(h)
+        return UTF8Decode(a)
+    
+    return decode(data[1:])
+
+def getChapterInfo(chapterURL):
+    print("getChapterInfo")
     try:
         response = urllib2.urlopen(baseURL + chapterURL)
         print(baseURL + chapterURL)
-        content = response.read()
+        content = response.read().decode("utf-8")
     except Exception, e:
         print(e)
         content = ""
 
-    imgPattern = re.compile(
-            r"<li style=\"width: 822px; height: 1200px;\">\S+<img src=\"(?P<imgURL>.*)\"")
-    imgURLs = imgPattern.findall(content)
-    print imgURLs
-    
+    dataPattern = re.compile(
+            r"var\s+DATA\s+=\s+'(?P<data>\S+)'")
+    data = dataPattern.search(content).group("data")
+    chapterInfo = json.loads(handleChapterInfo(data))
+    chapterTitle = chapterInfo["chapter"]["cTitle"]
+    chapterDirPath = os.path.join(baseDirPath, comicTitle, chapterTitle)
+    if not os.path.exists(chapterDirPath):
+        os.mkdir(chapterDirPath)
 
-def getComicChapterInfo(comicURL):
+    picUrls = []
+    for picInfo in chapterInfo["picture"]:
+        picUrls.append(picInfo["url"])
+    print(chapterTitle)
+    return chapterTitle, picUrls
+
+def getComicInfo(comicURL):
     try:
         response = urllib2.urlopen(comicURL)
-        content = response.read()
+        content = response.read().decode("utf-8")
     except Exception, e:
         print(e)
         return "", []
 
-    comicNamePattern = re.compile(
-            r"<h2 class=\"works-intro-title ui-left\"><strong>(?P<comicName>.*?)</strong></h2>")
-    result = comicNamePattern.search(content)
+    comicTitlePattern = re.compile(
+            r"<h2 class=\"works-intro-title ui-left\"><strong>(?P<comicTitle>.*?)</strong></h2>")
+    result = comicTitlePattern.search(content)
     if not result:
-        print("鏈壘鍒版极鐢绘爣棰橈紝璇风‘璁わ紒")
+        print("未找到漫画标题，请确认！")
         return "", []
-    comicName = result.group("comicName").decode("utf-8")
-    comicName = dealWithSpecialCharacter(comicName)
-    print(comicName)
+    comicTitle = result.group("comicTitle")
+    comicTitle = dealWithSpecialCharacter(comicTitle)
+    print(comicTitle)
 
-    comicDirPath = baseDirPath + comicName
-    #if not os.path.isabs(comicDirPath):
-    #    comicDirPath = os.path.abspath(comicDirPath)
+    comicChapterPattern = re.compile(
+            r"<a target=\"_blank\".*?href=\"(?P<URL>[/\w]+)\">\s")    #链接
+    chapterURLs = comicChapterPattern.findall(content)
+    if not chapterURLs:
+        print("未找到相关章节链接，请确认！")
+        return comicTitle, []
+    return comicTitle, chapterURLs
+
+if __name__ == "__main__":
+    comicId = 505430
+    comicId = input("输入漫画编号：")
+    comicURL = comicBaseURL + str(comicId)
+    comicTitle, chapterURLs = getComicInfo(comicURL)
+
+    comicDirPath = os.path.join(baseDirPath, comicTitle)
     if not os.path.exists(comicDirPath):
         os.mkdir(comicDirPath)
 
-    comicChapterPattern = re.compile(
-            r"<a target=\"_blank\".*?href=\"(?P<URL>[/\w]+)\">\s")    #閾炬帴
-    chapterURLs = comicChapterPattern.findall(content)
-    if not chapterURLs:
-        print("鏈壘鍒扮浉鍏崇珷鑺傞摼鎺ワ紝璇风‘璁わ紒")
-        return comicName, []
-    return comicName, chapterURLs
-
-if __name__ == "__main__":
-    comicURL = comicBaseURL + str(comicId)
-    comicName, chapterURLs = getComicChapterInfo(comicURL)
-    getChapterImgInfo(chapterURLs[1])
+    chapterTitle, picUrls = getChapterInfo(chapterURLs[0])
+    picIndex = 1
+    picDir = os.path.join(baseDirPath, comicTitle, chapterTitle)
+    for url in picUrls:
+        downloadPic(url, picDir, picIndex)
+        picIndex += 1
+        time.sleep(1)
